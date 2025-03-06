7baec039d47b23386194ba881e188af85a52628f
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-03-06 11:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let age = document.getElementById("age");
+let name = document.getElementById("name");
+let button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+	if(!age.value || !name.value){
+		alert("Please enter valid details");
+	}
+	else{
+		new Promise((resolve, reject) => {
+			setTimeout(() => {
+				if(age.value >= 18){
+					resolve(`Welcome, ${name.value}. you can vote.`)
+				}
+				else{
+					reject(`Oh sorry ${name.value}. you aren't old enough.`);
+				}
+		}, 2000);
+})
+.then(message => alert(message))
+.catch(error => alert(error));
+}
+});
